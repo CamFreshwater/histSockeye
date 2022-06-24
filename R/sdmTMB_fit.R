a1eda787f0269b244c420b99d06b9cd1a39d7925
CamFreshwater <ccfreshwater@gmail.com>
2022-06-24 07:45:00 -0700

    try fitting sdmTMB version - no luck

---
+++ R/sdmTMB_fit.R
@@ -0,0 +1,83 @@
+# Fit sdmTMB model to century of Nass data 
+# Faster alternative to brms_fit while still accounting for variable dispersion
+# 1) Import Skip's first century of data and clean as necessary
+# 2) Estimate temporal trend using GAM and brms to account for changes in
+# variability with sampling regime
+# Created by C Freshwater Dec 23, 2021
+# Update w/ edited data
+# -------------------------------------------------
+
+library(tidyverse)
+library(dplyr)
+library(forcats)
+library(sdmTMB)
+
+
+# import nass data
+dat_in <- read.table(here::here("data", "nasscenturyv3.txt"))
+colnames(dat_in) <- c("year", "month", "day", "hart_species", "sex", "age", 
+                      "fl")
+
+set.seed(123)
+dat <- dat_in %>%
+  mutate(
+    sex = ifelse(sex == 1, "male", "female") %>% as.factor(),
+    age = factor(as.character(age)),
+    year_f = as.factor(year),
+    age_f = age,
+    date = as.POSIXct(paste(day, month, year, sep = "-"),
+                      format = "%d-%m-%Y"),
+    yday = lubridate::yday(date),
+    yday_c = yday - mean(yday, na.rm = TRUE)
+  ) %>% 
+  #drop secondary age classes and years missing capture data
+  filter(
+    age %in% c("42", "52", "53", "63"),
+    !is.na(yday),
+    !is.na(fl)
+  )  %>%
+  # Add a factor representing sample collection to visualize effects 
+  mutate(
+    period = case_when(
+      year < 1957 ~ "Gilbert-Clemens",
+      year > 1972 & year < 1994 ~ "Monkley Dump",
+      year > 1993 ~ "Nisga'a",
+      TRUE ~ "Bilton"
+    ),
+    period = fct_relevel(as.factor(period), 
+                         "Gilbert-Clemens",
+                         "Bilton",
+                         "Monkley Dump",
+                         "Nisga'a"),
+    fl_cm = fl / 10,
+    # add random identifier to split datasets for subsetting,
+    data_group = sample.int(4, nrow(.), replace = T)
+  ) %>% 
+  droplevels()
+levels(dat$age_f) <- c("4[2]", "5[2]", "5[3]", "6[3]")
+
+
+# FIT MODEL  -------------------------------------------------------------------
+
+# parallelize based on operating system (should speed up some of the spatial
+# processing calculations)
+library(parallel)
+ncores <- detectCores() - 2
+if (Sys.info()['sysname'] == "Windows") {
+  library(doParallel)
+  cl <- makeCluster(ncores)
+  registerDoParallel(cl)
+} else {
+  doMC::registerDoMC(ncores)
+}
+
+dat_trim <- dat %>% sample_n(30000)
+
+# fit simple model
+fit0 <- sdmTMB(fl_cm ~ age + sex + period,
+               dispformula = ~ period,
+               data = dat_trim,
+               mesh = pcod_spde,
+               spatial = "off")
+
+## won't fit because no spatial data included
